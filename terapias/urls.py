53f3df43a8233a8cecd6518ebdfb6e53abbb003c
Mateo Palate <mpalate2620@uta.edu.ec>
2024-06-13 10:58:19 -0500

    save 2.3

--- terapias/urls.py
+++ terapias/urls.py
@@ -4,14 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('/menu/', views.terapia_paciente, name='terapia_paciente'),
+
     path('/<str:cedula>/', views.gestion_paciente, name='gestion_paciente'),
 
     path('movimientos/<int:terapia_id>/', views.movimientos, name='movimientos'),
+
     path('terapias/<int:rehabilitacion_id>/', views.terapias, name='terapias'), ##Esta es la nueva vista de terapias
 
     path('actualizar_sesiones/', views.actualizar_sesiones, name='actualizar_sesiones'),
     path('pacientes/', views.pacientes, name='pacientes'),
     path('/movimientos/editar/', views.gestion_movimiento, name='gestion_movimiento'),
+
     path('index/', views.index_view, name='index'),  # no cambiar
 
     path('TerapeutaOpciones/', views.TerapeutaOpciones_view, name='TerapeutaOpciones'),
--- terapias/views.py
+++ terapias/views.py
@@ -48,8 +48,8 @@ def dashboard_view(request):
     return render(request, 'dashboard.html')
 
 def terapia_paciente(request):
-    # Lógica de la vista de terapia paciente
-    return render(request, 'terapias.html')
+   # Lógica de la vista de terapia paciente
+  return render(request, 'terapias.html')
 
 def pacientes(request):
     # Lógica de la vista de pacientes
@@ -120,65 +120,7 @@ def rehabilitacion_paciente(request):
 
 #####################
 #Debes eliminar!!!!!
-def terapia_paciente(request):
-    paciente = None
-    terapias = None
-    cedula = request.POST.get('cedula', '') or request.GET.get('cedula', '')
-
-    if request.method == 'POST':
-        if 'buscar_paciente' in request.POST:
-            # Procesar búsqueda de paciente por cédula
-            cedula = request.POST.get('cedula', '')
-            if cedula:
-                paciente = get_object_or_404(Pacientes, cedula=cedula)
-                terapias = Terapias.objects.filter(paciente=paciente)
-
-        elif 'crear_terapia' in request.POST:
-            # Procesar creación de terapia para el paciente
-            paciente_id = request.POST.get('paciente_id', '')
-            if paciente_id:
-                paciente = get_object_or_404(Pacientes, pk=paciente_id)
-                # Aquí deberías tener la lógica para seleccionar un terapeuta, por ejemplo:
-                terapeuta = get_object_or_404(Fisioterapeutas, cedula="1850392620")
-                # Para simplificar, supongamos que ya tienes el terapeuta seleccionado
-
-                fecha_actual = datetime.date.today()
-                terapia = Terapias(fecha=fecha_actual, paciente=paciente, fisioterapeuta=terapeuta)
-                terapia.save()
-
-                for i in range(1, 6):
-                    movimiento = get_object_or_404(Movimientos, pk=i)
-                    sesion = Sesiones(movimientoID=movimiento, terapiaID=terapia, estado=False, porcentaje=0, repeticiones="15")
-                    sesion.save()
-
-                terapias = Terapias.objects.filter(paciente=paciente)
-
-
-
-                # Recargar las terapias actualizadas del paciente después de la creación
-                terapias = Terapias.objects.filter(paciente=paciente)
-
-        elif 'eliminar_terapia' in request.POST:
-            # Procesar eliminación de terapia
-            terapia_id = request.POST.get('eliminar_terapia', '')
-            paciente_id = request.POST.get('paciente_id', '')
-            if terapia_id and paciente_id:
-                terapia = get_object_or_404(Terapias, pk=terapia_id)
-                terapia.delete()
-
-                # Recargar las terapias actualizadas del paciente después de la eliminación
-                paciente = get_object_or_404(Pacientes, pk=paciente_id)
-                terapias = Terapias.objects.filter(paciente=paciente)
-                
-        elif 'limpiar_pantalla' in request.POST:
-            # Restablecer la pantalla a su estado inicial
-            paciente = None
-            terapias = None
-
-    return render(request, 'terapias.html', {
-        'paciente': paciente,
-        'terapias': terapias,
-    })
+#Terapia paciente
 
 #Terapias
 def terapias(request, rehabilitacion_id):
@@ -200,9 +142,11 @@ def terapias(request, rehabilitacion_id):
         
     elif 'eliminar_terapia' in request.POST:
             terapia_id = request.POST.get('terapia_id')
+            
             if terapia_id:
                 terapia = get_object_or_404(Terapias, pk=terapia_id)
                 terapia.delete()
+
                 return redirect('terapias', rehabilitacion_id=rehabilitacion_id)
 
     return render(request, 'terapias.html', {
